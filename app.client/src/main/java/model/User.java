86a8352f0622fd577a80233a426d6f9b61a3bca0
Grzegorz Kolano <grzechukol@gmail.com>
2019-01-22 02:27:28 +0100

    Add maven initial support.

---
+++ app.client/src/main/java/model/User.java
@@ -0,0 +1,74 @@
+package model;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity(name="users")
+@NamedNativeQuery(name = "User.byName", query = "select * from users where name = ?", resultClass = User.class)
+@org.hibernate.annotations.Entity(selectBeforeUpdate=true)
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+    private String name;
+    private Date joinDate;
+    @OneToOne
+    @JoinColumn(name="addressid")
+    private UsersAddress usersAddress;
+
+    @ManyToMany(cascade = {CascadeType.ALL})
+    @JoinTable(
+            name = "Attendance",
+            joinColumns = {@JoinColumn(name = "userid")},
+            inverseJoinColumns = {@JoinColumn(name = "eventid")}
+    )
+    Set<Event> events = new HashSet<>();
+
+    public Set<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(Set<Event> events) {
+        this.events = events;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(Date joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public UsersAddress getUsersAddress() {
+        return usersAddress;
+    }
+
+    public void setUsersAddress(UsersAddress usersAddress) {
+        this.usersAddress = usersAddress;
+    }
+
+    @Override
+    public String toString(){
+        return "Id= "+ userId + ", Name= " + name + ", {Address= " + usersAddress + "}";
+    }
+
+}
---
+++ jappo.server/src/main/java/model/SingleLog.java
@@ -0,0 +1,80 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity(name="SINGLE_LOG")
+public class SingleLog implements Serializable {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long logId;
+
+    private String labelOfTestedElement;
+    private Long timeOfExecution;
+    private Long timeOfResult;
+    private String uniqueProgramExecutionId;
+
+    public SingleLog(){}
+
+    public SingleLog(String label){
+        this.labelOfTestedElement = label;
+    }
+    public SingleLog(String label, Long timeOfExecution, Long timeOfResult){
+        this.labelOfTestedElement = label;
+        this.timeOfExecution = timeOfExecution;
+        this.timeOfResult = timeOfResult;
+    }
+
+    public SingleLog(String labelOfTestedElement, Long timeOfExecution, Long timeOfResult, String uniqueProgramExecutionId) {
+        this.labelOfTestedElement = labelOfTestedElement;
+        this.timeOfExecution = timeOfExecution;
+        this.timeOfResult = timeOfResult;
+        this.uniqueProgramExecutionId = uniqueProgramExecutionId;
+    }
+
+
+    @Override
+    public String toString() {
+        return (labelOfTestedElement + " "+timeOfExecution+" "+timeOfResult);
+    }
+
+    public String getUniqueProgramExecutionId() {return uniqueProgramExecutionId;    }
+
+    public void setUniqueProgramExecutionId(String uniqueProgramExecutionId) {
+        this.uniqueProgramExecutionId = uniqueProgramExecutionId;}
+
+    public Long getLogId() {
+        return logId;
+    }
+
+    public void setLogId(Long logId) {
+        this.logId = logId;
+    }
+
+    public Long getTimeOfExecution() {
+        return timeOfExecution;
+    }
+
+    public void setTimeOfExecution(Long timeOfExecution) {
+        this.timeOfExecution = timeOfExecution;
+    }
+
+    public Long getTimeOfResult() {
+        return timeOfResult;
+    }
+
+    public void setTimeOfResult(Long timeOfResult) {
+        this.timeOfResult = timeOfResult;
+    }
+
+    public String getLabelOfTestedElement() {
+        return labelOfTestedElement;
+    }
+
+    public void setLabelOfTestedElement(String labelOfTestedElement) {
+        this.labelOfTestedElement = labelOfTestedElement;
+    }
+}
